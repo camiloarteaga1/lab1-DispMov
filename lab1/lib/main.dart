452e3da0944ffacdd9cf5c68049c3ceed2711fc8
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2024-09-09 22:52:22 +0200

    Main file modificated to go to Home_screen

--- lab1/lib/main.dart
+++ lab1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -7,14 +8,20 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'App lab 1',
+
+      // locale: const Locale('es', 'CO'),
+
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
       ),
+      home: const HomeScreen(),
     );
   }
 }
